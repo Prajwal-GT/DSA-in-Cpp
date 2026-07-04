e310411bcfe2ffc18f1d911ecb807510f4f2ca17
Prajwal-GT <zorrovijay2@gmail.com>
2026-07-04 06:58:40 +0530

    Add files via upload

---
+++ GeekforGeeks/first and last sorted.cpp
@@ -0,0 +1,34 @@
+class Solution {
+  public:
+    vector<int> find(vector<int>& arr, int x) {
+        // code here
+        int start =0, end=arr.size()-1, mid;
+        int first=-1 ,last=-1;
+        while(start<=end)
+        {
+            mid=start+(end-start)/2;
+            if(arr[mid]==x){
+            first= mid;
+            end=mid-1;
+        }
+        else if(arr[mid]<x)
+        start =mid+1;
+        else
+        end =mid-1;
+        }
+    start =0, end=arr.size()-1;
+    while(start<=end)
+        {
+            mid=start+(end-start)/2;
+            if(arr[mid]==x){
+            last= mid;
+            start=mid+1;
+        }
+        else if(arr[mid]<x)
+        start =mid+1;
+        else
+        end =mid-1;
+        }
+        return {first, last};
+    }
+};
