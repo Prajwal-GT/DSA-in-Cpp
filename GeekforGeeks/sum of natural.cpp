605e858767a9bfdbc5c3ee26cd79fcda6b3fbe15
Prajwal-GT <zorrovijay2@gmail.com>
2026-07-04 06:57:51 +0530

    Add files via upload

---
+++ GeekforGeeks/sum of natural.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    // code here
+    int sum =0;
+
+for(int i =0; i<=n; i++){
+    sum=+i;
+}
+cout<<n*(n+1)/2;
+    return 0;
+}
